61aaa2402feb9e159efe63eb1b1d24f45a7759dd
tingtt <ting.taku@gmail.com>
2024-06-17 20:41:38 +0900

    feat(close): add customer count prompt

--- internal/usecase/close/prompt.go
+++ internal/usecase/close/prompt.go
@@ -3,8 +3,13 @@ package close
 import "hackbar-report/internal/usecase/open"
 
 type Prompt struct {
-	Income Income    `label:"収支" suffix:" ※収入はプラス、支出はマイナスで表記してください。"`
-	Cash   open.Cash `label:"レジ(各枚数)"`
+	CustomerCount Customers `label:"客数" suffix:"(\",\"または\"、\"区切りで複数入力)"`
+	Income        Income    `label:"収支" suffix:" ※収入はプラス、支出はマイナスで表記してください。"`
+	Cash          open.Cash `label:"レジ(各枚数)"`
+}
+
+type Customers struct {
+	Count string `label:"客数" mdblk-type:"list" mdblk-list-separate-with:",、"`
 }
 
 type Income struct {
